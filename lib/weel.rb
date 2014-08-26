f87577ba1ebf658dc4cc78da71c73cf24febc441
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2014-08-27 01:35:39 +0200

    simulation passes condition

--- lib/weel.rb
+++ lib/weel.rb
@@ -499,9 +499,10 @@ class WEEL
         return if __weel_is_in_search_mode
       end  
       if __weel_sim
-        hw, pos = __weel_sim_start(:loop,args.merge(:testing=>condition[1]))
+        cond = condition[0].is_a?(Proc) ? condition[0].call : condition[0]
+        hw, pos = __weel_sim_start(:loop,args.merge(:testing=>condition[1],:condition=>cond))
         yield
-        __weel_sim_stop(:loop,hw,pos,args.merge(:testing=>condition[1]))
+        __weel_sim_stop(:loop,hw,pos,args.merge(:testing=>condition[1],:condition=>cond))
         return
       end
       handlerwrapper = @__weel_handlerwrapper.new @__weel_handlerwrapper_args unless condition[0].is_a?(Proc)
